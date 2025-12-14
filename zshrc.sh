a0378f5155d8ab648dcf6705e4a0625a15560b29
Juan C. Yamacho H. <jcyamacho@gmail.com>
2025-12-14 16:08:40 +0100

    fix(zsh): fix zshrc plugin bootstrap and safe array sourcing

--- zshrc.sh
+++ zshrc.sh
@@ -17,11 +17,11 @@ typeset -gU fpath
 # CUSTOM_TOOLS_DIR
 export CUSTOM_TOOLS_DIR="${CUSTOM_TOOLS_DIR:-$HOME/.local/bin}"
 [[ -d $CUSTOM_TOOLS_DIR ]] || command mkdir -p -- "$CUSTOM_TOOLS_DIR"
-path=("$CUSTOM_TOOLS_DIR" $path)
+path=("$CUSTOM_TOOLS_DIR" "${path[@]}")
 # CUSTOM_TOOLS_DIR end
 
 # UTILS
-source "$ZDOTFILES_DIR/_utils.zsh"
+builtin source "$ZDOTFILES_DIR/_utils.zsh"
 # UTILS end
 
 # UPDATES
@@ -40,9 +40,10 @@ update-zdotfiles() {
 updates+=(_update_zdotfiles)
 
 update-all() {
+  local update
   for update in "${updates[@]}"; do
     "$update"
-    print
+    builtin print
   done
 
   reload
@@ -55,11 +56,11 @@ export ANTIDOTE_HOME="${ANTIDOTE_HOME:-$HOME/.cache/antidote}"
 [[ -d $ANTIDOTE_DIR ]] || command git clone --depth=1 https://github.com/mattmc3/antidote.git "$ANTIDOTE_DIR"
 
 # Lazy-load antidote from its functions directory.
-fpath=("$ANTIDOTE_DIR/functions" $fpath)
+fpath=("$ANTIDOTE_DIR/functions" "${fpath[@]}")
 autoload -Uz antidote
 
 # Set the root name of the plugins files (.txt and .zsh) antidote will use.
-local zsh_plugins="${ZDOTFILES_DIR}/.zsh_plugins"
+typeset zsh_plugins="${ZDOTFILES_DIR}/.zsh_plugins"
 
 # Generate a new static file whenever .zsh_plugins.txt is updated.
 if [[ ! "${zsh_plugins}.zsh" -nt "${zsh_plugins}.txt" ]]; then
@@ -68,7 +69,7 @@ if [[ ! "${zsh_plugins}.zsh" -nt "${zsh_plugins}.txt" ]]; then
 fi
 
 # Source your static plugins file.
-source "${zsh_plugins}.zsh"
+builtin source "${zsh_plugins}.zsh"
 unset zsh_plugins
 
 _antidote_update() {
