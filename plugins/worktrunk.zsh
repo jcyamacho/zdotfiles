49fe25bdb1a5d9dc3425714b29c5dfd69c920111
Juan C. Yamacho H. <jcyamacho@gmail.com>
2026-03-22 02:29:12 +0100

    feat(worktrunk): add config helper and shell aliases

--- plugins/worktrunk.zsh
+++ plugins/worktrunk.zsh
@@ -1,7 +1,19 @@
 # worktrunk (git worktree management): https://worktrunk.dev
 
 if exists wt; then
+  (( $+_worktrunk_config_file )) || typeset -gr _worktrunk_config_file="$HOME/.config/worktrunk/config.toml"
+
   source-cached-init wt config shell init zsh
+
+  alias wtl="wt list"
+  alias wtm="wt merge"
+  alias wts="wt switch"
+  alias wtcm="wt step commit"
+
+  wt-config() {
+    command mkdir -p -- "${_worktrunk_config_file:h}"
+    edit "$_worktrunk_config_file"
+  }
 
   if exists brew; then
     uninstall-worktrunk() {
