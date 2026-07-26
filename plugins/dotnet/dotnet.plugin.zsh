3202da07e4c08c9b0715b9e3024609e41be1add0
Juan C. Yamacho H. <jcyamacho@gmail.com>
2026-07-26 18:27:09 +0200

    refactor(dotnet): inline plugin paths
    Source .NET plugin files directly from the plugin directory without caching its path.

--- plugins/dotnet/dotnet.plugin.zsh
+++ plugins/dotnet/dotnet.plugin.zsh
@@ -1,7 +1,5 @@
 # .NET SDK (developer platform): https://dotnet.microsoft.com/
 
-(( $+_dotnet_plugin_dir )) || typeset -gr _dotnet_plugin_dir="$ZDOTFILES_DIR/plugins/dotnet"
+builtin source "$ZDOTFILES_DIR/plugins/dotnet/dotnet-sdk.zsh"
 
-builtin source "$_dotnet_plugin_dir/dotnet-sdk.zsh"
-
-exists dotnet && builtin source "$_dotnet_plugin_dir/dotnet-tools.zsh"
+exists dotnet && builtin source "$ZDOTFILES_DIR/plugins/dotnet/dotnet-tools.zsh"
