05d88d950dad678d680661876a6c0bceb0c39f6d
Juan C. Yamacho H. <jcyamacho@gmail.com>
2026-08-08 16:03:29 +0200

    refactor(bat): remove suffix aliases
    Remove automatic file-extension aliases that invoked bat.

--- plugins/bat.zsh
+++ plugins/bat.zsh
@@ -1,44 +1,6 @@
 # bat (cat clone with wings): https://github.com/sharkdp/bat
 
 if exists bat; then
-  # suffix aliases for popular file extensions
-  typeset -a _bat_exts=(
-    # text/docs
-    txt md markdown rst
-
-    # config
-    json jsonc yaml yml toml ini cfg conf xml
-
-    # shell
-    sh bash zsh fish
-
-    # web
-    html htm css scss sass less
-    js mjs cjs jsx ts mts cts tsx vue svelte
-
-    # systems
-    c h cpp hpp cc cxx rs go zig
-
-    # scripting
-    py pyw rb pl pm lua
-
-    # jvm
-    java kt kts scala groovy gradle
-
-    # data
-    csv sql graphql
-
-    # devops
-    dockerfile tf hcl
-
-    # misc
-    diff patch log env
-  )
-  for _ext in "${_bat_exts[@]}"; do
-    alias -s "$_ext"="bat"
-  done
-  unset _ext _bat_exts
-
   if exists brew; then
     uninstall-bat() {
       info "Uninstalling bat..."
